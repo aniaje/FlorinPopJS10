f21ef6cfee66cd7027b92c9f1ba3a77435096860
Anna Jeżowska <annaje@Annas-MacBook-Air.local>
2022-06-19 16:51:22 +0200

    footer

---
+++ Workspace/flahute/scripts.js
@@ -0,0 +1,106 @@
+const triggerTabList = document.querySelectorAll("#myTab button");
+triggerTabList.forEach((triggerEl) => {
+  const tabTrigger = new bootstrap.Tab(triggerEl);
+
+  triggerEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    tabTrigger.show();
+  });
+});
+
+const images = document.querySelectorAll(".gallery__item img");
+let imgSrc;
+// get images src onclick
+images.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    imgSrc = e.target.src;
+    //run modal function
+    imgModal(imgSrc);
+  });
+});
+//creating the modal
+let imgModal = (src) => {
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  //add the modal to the main section or the parent element
+  document.querySelector(".main").append(modal);
+  //adding image to modal
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", src);
+  //creating the close button
+  const closeBtn = document.createElement("i");
+  closeBtn.setAttribute("class", "fas fa-times closeBtn");
+  //close function
+  closeBtn.onclick = () => {
+    modal.remove();
+  };
+  modal.append(newImage, closeBtn);
+};
+
+//counter
+
+// document.addEventListener("DOMContentLoaded", updated, false);
+
+// let counts = setInterval(updated);
+// let upto = 0;
+
+// function updated() {
+//   let count = document.getElementById("counter-1");
+//   count.innerHTML = ++upto;
+//   if (upto === 252) {
+//     clearInterval(counts);
+//   }
+// }
+
+// let count2 = document.getElementById("counter-2");
+
+//bike enters
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    // half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+const faceImg = document.querySelector(".face-img");
+
+console.log(faceImg);
+
+faceImg.addEventListener("click", enlarge);
+
+function enlarge(e) {
+  console.log("click");
+  console.log(e.target);
+  e.target.classList.add = "enlarged";
+}
